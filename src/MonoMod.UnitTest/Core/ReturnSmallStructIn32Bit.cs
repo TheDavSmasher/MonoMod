c1fc589b5c9d8078c2099b124983e4351868affe
Andreas Pardeike <andreas@pardeike.net>
2025-04-22 00:56:19 +0200

    tests detour of 32bit instance method with params returning 2 byte struct

---
+++ src/MonoMod.UnitTest/Core/ReturnSmallStructIn32Bit.cs
@@ -0,0 +1,48 @@
+﻿using MonoMod.Core;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MonoMod.UnitTest.Core
+{
+    public class ReturnSmallStructIn32Bit : TestBase
+    {
+        public ReturnSmallStructIn32Bit(ITestOutputHelper helper) : base(helper)
+        {
+        }
+
+        private struct St02
+        {
+            public byte b1;
+            public byte b2;
+        }
+
+        private class Clazz
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public St02 Original(string s)
+            {
+                Console.WriteLine("Original should never be called (" + s + ")");
+                return new St02();
+            }
+
+            public static St02 Replacement(Clazz _, string s)
+            {
+                Console.WriteLine("Replacement called with " + s);
+                return new St02();
+            }
+        }
+
+        [Fact]
+        public void ReturningSmallStructInInstanceMethodWithParameters_DoesNotThrow()
+        {
+            var all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+            var method = typeof(Clazz).GetMethod(nameof(Clazz.Original), all);
+            var replacement = typeof(Clazz).GetMethod(nameof(Clazz.Replacement), all);
+            using var result = DetourFactory.Current.CreateDetour(method, replacement);
+            _ = (new Clazz()).Original("test");
+        }
+    }
+}
